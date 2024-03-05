b33084b2ea88b4f071dabb0d92d13c8efe46e325
lum-jashari <lumijashari@hotmail.com>
2024-03-05 15:49:15 +0100

    project setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="flex flex-col h-screen max-h-screen">
+      <!-- search / results -->
+      <div class="flex justify-center relative px-4 pt-8 pb-32"></div>
+      <div></div>
+    </div>
+  </main>
+</template>
