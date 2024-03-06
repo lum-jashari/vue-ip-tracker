7e7f0d430ee7d1dc44c6989e128a1d94735696ed
lum-jashari <lumijashari@hotmail.com>
2024-03-06 14:18:26 +0100

    added search input and ip info component

---
+++ src/components/IPInfo.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+
+<template>
+  <div
+    class="absolute -bottom-64 lg:-bottom-20 flex flex-col lg:flex-row bg-white gap-y-4 gap-x-12 px-8 pt-8 pb-12 rounded-md"
+  >
+    <!-- flex item #1 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">IP Address</h3>
+      <span class="text-2xl">8.8.8.8</span>
+    </div>
+    <!-- flex item #2 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Location</h3>
+      <span class="text-2xl">Michigan</span>
+    </div>
+    <!-- flex item #3 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">Timezone</h3>
+      <span class="text-2xl">UTC -07:00</span>
+    </div>
+    <!-- flex item #4 -->
+    <div class="flex flex-col">
+      <h3 class="text-xs mb-2 uppercase">ISP</h3>
+      <span class="text-2xl">Google</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,33 @@
-<script setup></script>
+<script setup>
+import IPInfo from "@/components/IPInfo.vue";
+</script>
 
 <template>
   <main>
     <div class="flex flex-col h-screen max-h-screen">
       <!-- search / results -->
-      <div class="flex justify-center relative px-4 pt-8 pb-32"></div>
-      <div></div>
+      <div
+        class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+      >
+        <!-- search input -->
+        <div class="w-full max-w-screen-sm">
+          <h1 class="text-white text-center text-3xl pb-4">
+            IP Address Tracker
+          </h1>
+          <div class="flex">
+            <input
+              class="flex-1 py-3 pl-2 rounded-tl-md rounded-bl-md focus:outline-none"
+              type="text"
+              placeholder="Search for any IP address or leave it empty to get your IP info"
+            />
+            <i
+              class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fa-solid fa-chevron-right"
+            ></i>
+          </div>
+        </div>
+        <!-- ip info -->
+        <IPInfo />
+      </div>
     </div>
   </main>
 </template>
